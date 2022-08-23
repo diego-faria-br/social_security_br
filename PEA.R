f856cda3bd36b4fea43e9543a58d9a3afd00a419
diego-faria-br <diegoliveirafaria@gmail.com>
2022-08-23 19:08:47 +0200

    PEA 2021, ajustes para maior de 100 anos

--- PEA.R
+++ PEA.R
@@ -3,12 +3,19 @@ library(survey)
 library(srvyr)
 library(tidyr)
 library(dplyr)
+library(multidplyr)
 library(kableExtra)
 library(ggplot2)
+
 rm(list = ls())
 SM_2019 <- 998
 
-dados_srvyr_2021<-as_survey(pnadc_design(read_pnadc("./2021/PNADC_042021.txt","./input/input_PNADC_trimestral.txt",vars=c(
+
+# 2021 --------------------------------------------------------------------
+
+
+
+dados_srvyr_2021<-as_survey(pnadc_design(read_pnadc("./PNADC_042021.txt","./input/input_PNADC_trimestral.txt",vars=c(
   'V1022', # Situação do Domicílio: 1- Urbano 2-Rural
   'V2005', #Situação da pessoa no domicílio'01'-Pessoa responsavel pelo domicilio/02-Conjuge/03-Conjuge
   'V2007', # Sexo
@@ -18,21 +25,35 @@ dados_srvyr_2021<-as_survey(pnadc_design(read_pnadc("./2021/PNADC_042021.txt",".
   ))))
 
 
+# Define PEA como sendo a população ocupada ou desocupada
 dados_srvyr_2021<- dados_srvyr_2021 %>% mutate(PEA = if_else(VD4002 %in% c('1','2'),1,0))
 
-x2021 <- dados_srvyr_2021 %>% group_by(V2009,V2007,V1022) %>% summarise(PEA_Total = survey_total(PEA))
+PEA_H_Urb_2021 <- PEA_H_Urb_2021 %>% select(!PEA_Total_se) %>%  mutate(PEA_Total=round(PEA_Total,0))
 
-x2021 <-x2021 %>% mutate(PEA_Total=trunc(PEA_Total))
-x2021 <- x2021 %>% mutate(PEA_Total=trunc(PEA_Total)) %>% select(!PEA_Total_se)
-
-names(x2021) <- c('idade','sexo','clientela','pea.total')
-
-PEA_H_Urb <-x2021 %>% filter(sexo=='1',clientela=='1')
-PEA_H_Rur <-x2021 %>% filter(sexo=='1',clientela=='2')
-PEA_M_Urb <-x2021 %>% filter(sexo=='2',clientela=='1')
-PEA_M_Rur <-x2021 %>% filter(sexo=='2',clientela=='2')
+PEA_H_Urb_2021 <- dados_srvyr_2021 %>% filter(V2007=='1',V1022=='1') %>% group_by(V2009) %>% summarise(PEA_Total =survey_total(PEA)) %>% select(!PEA_Total_se) %>%  mutate(PEA_Total=round(PEA_Total,0))
+PEA_H_Rur_2021 <- dados_srvyr_2021 %>% filter(V2007=='1',V1022=='2') %>% group_by(V2009) %>% summarise(PEA_Total =survey_total(PEA)) %>% select(!PEA_Total_se) %>%  mutate(PEA_Total=round(PEA_Total,0))
+PEA_M_Urb_2021 <- dados_srvyr_2021 %>% filter(V2007=='2',V1022=='1') %>% group_by(V2009) %>% summarise(PEA_Total =survey_total(PEA)) %>% select(!PEA_Total_se) %>%  mutate(PEA_Total=round(PEA_Total,0))
+PEA_M_Rur_2021 <- dados_srvyr_2021 %>% filter(V2007=='2',V1022=='2') %>% group_by(V2009) %>% summarise(PEA_Total =survey_total(PEA)) %>% select(!PEA_Total_se) %>%  mutate(PEA_Total=round(PEA_Total,0))
 
 
+mais_de_100 <- function(pea_Data){
+  idade_cent <-  pea_Data %>% mutate(idade_cent = if_else(V2009>=100,1,0)) %>% group_by(
+    idade_cent) %>% summarise(V2009,PEA_Total,total_cent = sum(PEA_Total)) %>% select(
+      V2009,PEA_Total,idade_cent,total_cent)
+  
+  pea_Data <-  bind_cols(pea_Data,idade_cent$total_cent)
+  
+  pea_Data <- pea_Data %>% filter(V2009<=100) %>% mutate(PEA_Total = if_else(V2009==100,`...3`,PEA_Total)) %>% select(V2009,PEA_Total)
+  return(pea_Data)
+}
+
+PEA_H_Urb_2021<- mais_de_100(PEA_H_Rur_2021)
+PEA_H_Rur_2021<- mais_de_100(PEA_H_Rur_2021)
+PEA_M_Urb_2021<- mais_de_100(PEA_H_Rur_2021)
+PEA_M_Rur_2021<- mais_de_100(PEA_H_Rur_2021)
 
 
+# 2020 --------------------------------------------------------------------
+
+  
 
